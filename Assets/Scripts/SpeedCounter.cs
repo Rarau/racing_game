c8874415e6efeae4cee0eb022082837c2adbbee0
Raul <rarau001@campus.goldsmiths.ac.uk>
2016-02-06 20:24:39 +0100

    Initial setup and assets for prototyping Starting with some physics tests

---
+++ Assets/Scripts/SpeedCounter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class SpeedCounter : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    private Text t;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.text = (rb.velocity.magnitude * 3.6f).ToString("0.0 KMH");
+    }
+}
---
+++ Assets/Scripts/StraightLineTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StraightLineTest : MonoBehaviour {
+
+    public float engineForce = 10.0f;   // Constant force from the engine
+    public float cDrag;                 // Drag (air friction constant)
+    public float cRoll;                 // Rolling resistance (wheel friction constant)
+
+    public float carMass = 1200.0f;     // Mass of the car in Kilograms
+
+    Rigidbody rigidbody;
+
+    bool accel;
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+    void Update()
+    {
+        accel = Input.GetKey(KeyCode.W);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 velocity = rigidbody.velocity;
+        Vector3 fTraction = transform.forward * (accel ? engineForce : 0.0f);
+        Vector3 fDrag = -cDrag * velocity * velocity.magnitude;
+        Vector3 fRoll = -cRoll * velocity;
+
+        Vector3 fLong = fTraction + fDrag + fRoll;  // Total longitudinal force
+
+        Vector3 acceleration = fLong / carMass;
+
+        rigidbody.velocity += acceleration * Time.deltaTime;
+
+	}
+}
