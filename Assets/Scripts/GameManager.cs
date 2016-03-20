5d6ac122f7651f7bfb79f69a2c0df975db3b3455
GonLou <goncalo.lourenco@outlook.com>
2016-03-20 21:09:31 +0000

    Basic game manager build.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
-public class Game : MonoBehaviour {
+public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private static GameManager _instance;
+
+    public enum LevelState { SPLASH_SCREEN, MAIN_MENU, CONFIG, RACE, FINISH_GRID };
+    public enum GameState  { START, PAUSE, CANCEL, END };
+
+    public int numberOfHumanPlayers;
+    public string[] nameOfPlayers;
+    public int trackSelected;
+    public bool[] carSelected;
+
+    // to hold player data when pauses
+    struct PlayerData
+    {
+        float speed;
+        Vector3 position;
+        int gear;
+    }
+    List<PlayerData> playerData = new List<PlayerData>();
+
+    public static GameManager Instance
+    {
+        get
+        {
+            //create logic to create the instance
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    void Start()
+    {
+        //SceneManager.LoadScene(LevelState.SPLASH_SCREEN.ToString());
+        numberOfHumanPlayers = 0;
+        for (int i=0; i<4; i++)
+        {
+            nameOfPlayers[i] = string.Concat("Player 0", i.ToString());
+            carSelected[i] = false;
+        }
+
+        trackSelected = 0;
+    }
+
+    string getLevel()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    void setLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    void Awake ()
+    {
+        _instance = this;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Sets the instance to null when the application quits
+    public void OnApplicationQuit()
+    {
+        _instance = null;
+    }
 }
