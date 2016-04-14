0362df615eed52225772820b0c444ad3f9940b57
Raul <rarau001@campus.goldsmiths.ac.uk>
2016-04-14 20:37:57 +0200

    Fixed wheel fx

--- Assets/Scripts/FX/WheelFX.cs
+++ Assets/Scripts/FX/WheelFX.cs
@@ -2,17 +2,21 @@
 using System.Collections;
 
 //[RequireComponent(typeof(WheelController))]
-public class WheelFX : MonoBehaviour {
+public class WheelFX : MonoBehaviour
+{
 
     public WheelController wheel;
 
     public ParticleSystem particles;
 
     public GameObject skidmarkPrefab;
+    private GameObject skidmarkObject;
+
     public AudioSource skidAudio;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         wheel = wheel == null ? GetComponent<WheelController>() : wheel;
         particles.playOnAwake = false;
         particles.enableEmission = false;
@@ -21,7 +25,7 @@ public class WheelFX : MonoBehaviour {
         if (skidAudio == null)
         {
             Debug.LogError("No audio source, please add one skid audio to the tyres");
-           // enabled = false;
+            // enabled = false;
         }
         if (skidmarkPrefab == null)
         {
@@ -31,21 +35,26 @@ public class WheelFX : MonoBehaviour {
         else
         {
             skidmarkPrefab.SetActive(false);
+            skidmarkObject = GameObject.Instantiate(skidmarkPrefab);
+            skidmarkObject.transform.parent = transform;
+            skidmarkObject.transform.localPosition = -Vector3.up * 0.27f;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if ((Mathf.Abs(wheel.slipAngle) > 20.0f || Mathf.Abs(wheel.slipRatio) > 0.10f) && Mathf.Abs(wheel.rpm) > 2.10f)
         {
             //Debug.Log("ON " + name);
             ParticleSystem.EmissionModule em = particles.emission;
             particles.Play();
             em.enabled = true;
-
-            //particles.enableEmission = true;
-            skidmarkPrefab.SetActive(true);
-            //skidAudio.Play();
+            if (wheel.IsGrounded)
+            {
+                skidmarkObject.SetActive(true);
+            }
+            if (!skidAudio.isPlaying) skidAudio.Play();
         }
         else
         {
@@ -53,10 +62,8 @@ public class WheelFX : MonoBehaviour {
             ParticleSystem.EmissionModule em = particles.emission;
             particles.Stop();
             em.enabled = false;
-            
+            skidmarkObject.SetActive(false);
 
-           // particles.enableEmission = false;
-            skidmarkPrefab.SetActive(false);
-        } 
+        }
     }
 }
