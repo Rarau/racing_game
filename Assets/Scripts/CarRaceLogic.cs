b5db54a3757078c7329634faf66cd9744837a3cf
Adam Joyce <adamjoyce@outlook.com>
2016-04-25 00:00:00 +0100

    Car now must travel in the correct direction to complete laps

--- Assets/Scripts/CarRaceLogic.cs
+++ Assets/Scripts/CarRaceLogic.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CarRaceLogic : MonoBehaviour {
 
     public int lap = 0;
-    public int lastCheckpoint = 0;
+    public int lastCheckpoint = -1;
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -12,7 +12,13 @@ public class Checkpoint : MonoBehaviour {
 	private void OnTriggerEnter(Collider carCollider) {
         if (carCollider.tag == "CheckpointCollider") {
             Transform car = carCollider.gameObject.transform.root;
+
+            // Enforce that the car travels in the correct direction.
             if (car.GetComponent<CarRaceLogic>().lastCheckpoint == thisCheckpoint - 1) {
+                car.GetComponent<CarRaceLogic>().lastCheckpoint = thisCheckpoint;
+            } else if (car.GetComponent<CarRaceLogic>().lastCheckpoint == 7 && thisCheckpoint == 0) {
+                // The car has completed a lap.
+                car.GetComponent<CarRaceLogic>().lap++;
                 car.GetComponent<CarRaceLogic>().lastCheckpoint = thisCheckpoint;
             } else {
                 Debug.Log("Turn around you are travelling in the wrong direction.");
