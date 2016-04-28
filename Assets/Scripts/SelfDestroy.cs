2d4dfced253ff0cf06d94e67f52e808c5780b97d
Adam Joyce <adamjoyce@outlook.com>
2016-04-28 16:19:03 +0100

    Got skidmarks working correctly on the newWheels prefab

---
+++ Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestroy : MonoBehaviour {
+    public float lifetime = 2.0f;
+    private float startTime;
+
+    void OnEnable() {
+        startTime = Time.timeSinceLevelLoad;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if ((startTime + lifetime) < Time.timeSinceLevelLoad)
+            Destroy(gameObject);
+    }
+}
