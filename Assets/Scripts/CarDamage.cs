6b65feabf48e699fd287e1c38b111af7d5a80635
GonLou <goncalo.lourenco@outlook.com>
2016-05-02 14:40:39 +0100

    Damage by mesh not working

---
+++ Assets/Scripts/CarDamage.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDamage : MonoBehaviour {
+
+    //private CarController carController;
+
+    public Mesh[] swapMesh;
+
+    public int carHealth;
+
+    private MeshFilter meshFilter;
+
+    // Use this for initialization
+    void Start () {
+        //carController = transform.root.GetComponent<CarController>();
+
+        carHealth = 100;
+
+        meshFilter = this.GetComponent<MeshFilter>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Car Health" + carHealth);
+
+        if (carHealth < 100)
+        {
+            meshFilter.mesh = swapMesh[1];
+        }
+        else
+        {
+            meshFilter.mesh = swapMesh[0];
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enviroment")
+        {
+            Debug.Log("something hit");
+            carHealth -= 5;
+        }
+        Debug.Log("collision");
+        carHealth -= 10;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+            Destroy(other);
+    }
+}
---
+++ Assets/Scripts/MeshChange.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshChange : MonoBehaviour {
+
+    public float meshHealth;
+
+    void Start()
+    {
+        gameObject.transform.GetChild(0).GetComponent<Renderer>().enabled = true;
+        gameObject.transform.GetChild(1).GetComponent<Renderer>().enabled = false;
+        meshHealth = 100;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (meshHealth < 100)
+        {
+            gameObject.transform.GetChild(0).GetComponent<Renderer>().enabled = false;
+            gameObject.transform.GetChild(1).GetComponent<Renderer>().enabled = true;
+
+        }
+        else 
+        {
+            gameObject.transform.GetChild(0).GetComponent<Renderer>().enabled = true;
+            gameObject.transform.GetChild(1).GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
